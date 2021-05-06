bcf9814e74b43b6330951255ad0afa162a2aa167
Maria M <prov21_1@hotmail.com>
2021-05-05 19:42:46 -0700

    post routes and model

---
+++ models/Post.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Post extends Model {}
+Post.init(
+  {
+    // need category?
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    location: {
+      type: DataTypes.STRING,
+      references: {
+        model: "user",
+        key: "location",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
+
+module.exports = Post;
