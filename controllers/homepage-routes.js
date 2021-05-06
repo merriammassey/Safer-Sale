6038c960902b38cad85a8d64e8b73b44f152943e
Breedlove13 <brianbreedlove0013@gmail.com>
2021-05-05 20:32:12 -0700

    hdhd

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -2,7 +2,16 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.render("homepage");
+  res.render("homepage", {
+    id: 1,
+    product_name: 'testing product', 
+    price: 25.00,
+    created_at: new Date(), 
+    comments: [{}, {}],
+    user: {
+      username: 'ultimate_test'
+    }
+    });
 });
 
 router.get("/login", (req, res) => {
