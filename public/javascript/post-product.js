2c4472cd4fda4318f051770ddda04f3bbee39ecc
Breedlove13 <brianbreedlove0013@gmail.com>
2021-05-07 16:25:32 -0700

    input in for new product, is going back to database and the returning on the correct path

--- public/javascript/post-product.js
+++ public/javascript/post-product.js
@@ -1,8 +1,9 @@
 async function newFormHandler(event) {
   event.preventDefault();
 
-  const title = document.querySelector('input[name="post-title"]').value;
-  const post_url = document.querySelector('input[name="post-url"]').value;
+  const title = document.querySelector('input[name="item-post"]').value;
+  const description = document.querySelector('input[name="description-post"]').value
+  const price = document.querySelector('input[name="price-post"]').value;
 
   const response = await fetch(`/api/posts`, {
     method: "POST",
@@ -15,7 +16,7 @@ async function newFormHandler(event) {
       "Content-Type": "application/json",
     },
   });
-
+  console.log(response)
   if (response.ok) {
     document.location.replace("/dashboard");
   } else {
