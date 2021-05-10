958fff684e443b389e03298cfb53ec2da5858fce
Breedlove13 <brianbreedlove0013@gmail.com>
2021-05-10 14:30:21 -0700

    sign up with location and sign in session wroking

---
+++ public/javascript/signup.js
@@ -0,0 +1,31 @@
+async function signupFormHandler(event) {
+  event.preventDefault();
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+  const location = document.querySelector("#location-signup").value.trim()
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "Post",
+      body: JSON.stringify({
+        username,
+        email,
+        location,
+        password,
+        
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    //check the response status
+    if (response.ok) {
+      console.log(response);
+      console.log("success");
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+
+document.querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
