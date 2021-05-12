57c5def37469fee904705c187d0f881a66f67c19
Shivvers <ashivvers95@gmail.com>
2021-05-12 13:15:01 -0700

    places api almost completed

--- public/javascript/places-api.js
+++ public/javascript/places-api.js
@@ -1,48 +1,68 @@
-var inputSearch = document.getElementById('input-searched-city');
+var inputSearch = document.getElementById('searchBar');
 
 var map;
+
 var selectAutocompleteOptions = {
+    location: inputSearch,
+    radius: '500',
     types: ['(library)']
 };
 
-map = new google.maps.Map(document.getElementById('map'), {
-    center: new google.maps.LatLng(-33.8665433, 151.1956316),
-    zoom: 15
-});
+// map = new google.maps.Map(document.getElementById('map'), {
+//     center: new google.maps.LatLng(-33.8665433, 151.1956316),
+//     zoom: 15
+// });
 
-selectAutocomplete = new google.maps.places.Autocomplete(inputSearch, selectAutocompleteOptions);
-selectAutocomplete.bindTo('bounds', map);
+// selectAutocomplete = new google.maps.places.Autocomplete(inputSearch, selectAutocompleteOptions);
+// selectAutocomplete.nearbySearch(selectAutocompleteOptions, callback);
 
-google.maps.event.addListener(selectAutocomplete, 'place_changed', function () {
-    console.log(selectAutocomplete.getPlace());
-});
+// google.maps.event.addListener(selectAutocomplete, 'searchButton', function () {
+//     console.log(results);
+//});
+
+
+fetch('https://thingproxy.freeboard.io/fetchhttps://maps.googleapis.com/maps/api/place/textsearch/json?query=library+in+'
+ + inputSearch.value + 
+ '&key=AIzaSyBBlNIHoJ1O1S0XMqeTTKXXISgTn6y3rbA')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+  fetch('https://thingproxy.freeboard.io/fetchhttps://maps.googleapis.com/maps/api/place/textsearch/json?query=shopping_mall+in+'
+  + inputSearch.value + 
+  '&key=AIzaSyBBlNIHoJ1O1S0XMqeTTKXXISgTn6y3rbA')
+   .then(response => response.json())
+   .then(data => console.log(data));
+
+   fetch('https://thingproxy.freeboard.io/fetchhttps://maps.googleapis.com/maps/api/place/textsearch/json?query=police_station+in+'
+   + inputSearch.value + 
+   '&key=AIzaSyBBlNIHoJ1O1S0XMqeTTKXXISgTn6y3rbA')
+    .then(response => response.json())
+    .then(data => console.log(data));
+
 
 // var map;
 // var service;
 // var infowindow;
 
 // function initialize() {
-//   var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
 
 //   map = new google.maps.Map(document.getElementById('map'), {
-//       center: pyrmont,
+//       center: new google.maps.LatLng(-33.8665433,151.1956316),
 //       zoom: 15
 //     });
 
 //   var request = {
-//     location: pyrmont,
-//     radius: '500',
-//     type: ['restaurant']
+//     type: ['library']
 //   };
 
 //   service = new google.maps.places.PlacesService(map);
 //   service.nearbySearch(request, callback);
 // }
 
-// function callback(results, status) {
-//   if (status == google.maps.places.PlacesServiceStatus.OK) {
-//     for (var i = 0; i < results.length; i++) {
-//       createMarker(results[i]);
-//     }
-//   }
-// }
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
