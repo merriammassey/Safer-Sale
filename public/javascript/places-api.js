5e89a73ede64639ce566eae8d88a233328c4ff2a
Shivvers <ashivvers95@gmail.com>
2021-05-12 18:22:15 -0700

    will console.log all querried data from api, will not print ot page

--- public/javascript/places-api.js
+++ public/javascript/places-api.js
@@ -1,4 +1,6 @@
-var inputSearch = document.getElementById("#searchBar");
+//const router = require('express').Router();
+
+var inputSearch = document.querySelector('#searchBar');
 var libraryButton = document.querySelector(".library-searchButton");
 var mallButton = document.querySelector(".shopping_mall-searchButton");
 var policeButton = document.querySelector(".police_station-searchButton");
@@ -9,55 +11,62 @@ var openLocation = document.querySelector(".open");
 
 libraryButton.addEventListener("click", function () {
   fetch(
-    "https://thingproxy.freeboard.io/fetchhttps://maps.googleapis.com/maps/api/place/textsearch/json?query=library+in+" +
+    "https://thingproxy.freeboard.io/fetch/https://maps.googleapis.com/maps/api/place/textsearch/json?query=library+in" +
       inputSearch.value +
       "&key=AIzaSyBBlNIHoJ1O1S0XMqeTTKXXISgTn6y3rbA"
   )
     .then((response) => response.json())
     .then((data) => {
-      var nameValue = data["results"]["name"];
-      var addressValue = data["results"]["formatted_address"];
-      var openValue = data["results"]["opening_hours"]["open_now"];
+        console.log(data);
+      const nameValue = data["results"][0]["name"];
+      const addressValue = data["results"][0]["formatted_address"];
+      const openValue = data["results"][0]["opening_hours"]["open_now"];
 
-      nameLocation.innerHtml = "Location name: " + nameValue;
-      address.innerHtml = " Location address: " + addressValue;
-      openLocation.innerHtml = "Location open: " + openValue;
+    //   nameLocation.innerHtml = "Location name: " + nameValue;
+    //   address.innerHtml = "Location address: " + addressValue;
+    //   openLocation.innerHtml = "Location open: " + openValue;
+    
+        console.log(nameValue, addressValue, openValue)
     });
 });
 
 mallButton.addEventListener("click", function () {
   fetch(
-    "https://thingproxy.freeboard.io/fetchhttps://maps.googleapis.com/maps/api/place/textsearch/json?query=shopping_mall+in+" +
+    "https://thingproxy.freeboard.io/fetch/https://maps.googleapis.com/maps/api/place/textsearch/json?query=shopping_mall+in" +
       inputSearch.value +
       "&key=AIzaSyBBlNIHoJ1O1S0XMqeTTKXXISgTn6y3rbA"
   )
     .then((response) => response.json())
     .then((data) => {
-      var nameValue = data["results"]["name"];
-      var addressValue = data["results"]["formatted_address"];
-      var openValue = data["results"]["opening_hours"]["open_now"];
+      const nameValue = data["results"]["name"];
+      const addressValue = data["results"]["formatted_address"];
+      const openValue = data["results"]["opening_hours"]["open_now"];
 
-      nameLocation.innerHtml = "Location name: " + nameValue;
-      address.innerHtml = " Location address: " + addressValue;
-      openLocation.innerHtml = "Location open: " + openValue;
+    //   nameLocation.innerHtml = "Location name: " + nameValue;
+    //   address.innerHtml = " Location address: " + addressValue;
+    //   openLocation.innerHtml = "Location open: " + openValue;
+
+    console.log(nameValue, addressValue, openValue)
     });
 });
 
 policeButton.addEventListener("click", function () {
   fetch(
-    "https://thingproxy.freeboard.io/fetchhttps://maps.googleapis.com/maps/api/place/textsearch/json?query=police_station+in+" +
-      inputSearch.value +
+    "https://thingproxy.freeboard.io/fetch/https://maps.googleapis.com/maps/api/place/textsearch/json?query=police_station+in" 
+    + inputSearch.value +
       "&key=AIzaSyBBlNIHoJ1O1S0XMqeTTKXXISgTn6y3rbA"
   )
     .then((response) => response.json())
     .then((data) => {
-      var nameValue = data["results"]["name"];
-      var addressValue = data["results"]["formatted_address"];
-      var openValue = data["results"]["opening_hours"]["open_now"];
+      const nameValue = data["results"]["name"];
+      const addressValue = data["results"]["formatted_address"];
+      const openValue = data["results"]["opening_hours"]["open_now"];
 
-      nameLocation.innerHtml = "Location name: " + nameValue;
-      address.innerHtml = " Location address: " + addressValue;
-      openLocation.innerHtml = "Location open: " + openValue;
+    //   nameLocation.innerHtml = "Location name: " + nameValue;
+    //   address.innerHtml = " Location address: " + addressValue;
+    //   openLocation.innerHtml = "Location open: " + openValue;
+
+    console.log(nameValue, addressValue, openValue)
     });
 });
 
